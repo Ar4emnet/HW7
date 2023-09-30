fdd3a4a5a3e6e38707c296a12329684b626ad3c0
Artem <artemden.kolesnik@gmail.com>
2023-09-30 13:51:33 +0300

    did HW7.2

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ def power(number,step):
     else:
         return number * power(number,step - 1)
 
-print(power(4,4))
+
+def star_print(number):
+    if number == 1:
+        return "*"
+    else:
+        return "*" + str(star_print(number-1))
+
+
+print(star_print(8))
+print(power(4, 4))
