349aa4175a5cfd248a8c8e8abbaace327e3830a8
Artem <artemden.kolesnik@gmail.com>
2023-09-30 13:55:07 +0300

    did HW7.3

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ def star_print(number):
         return "*" + str(star_print(number-1))
 
 
+def sum_numbers(a, b):
+    if a == b:
+        return a
+    else:
+        return a + sum_numbers(a+1, b)
+
+print(sum_numbers(1,5))
 print(star_print(8))
 print(power(4, 4))
